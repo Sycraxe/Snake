1505ad7ad67677962af26f93fa6a66996f2595c0
Sycraxe <sycraxe@gmail.com>
2024-02-16 15:25:57 +0100

    feat: make game playable

--- __main__.py
+++ __main__.py
@@ -1,20 +1,14 @@
 from __future__ import annotations
+from typing import List
+from random import randint
 import pygame
 
-W, H = 19, 19
+width, height = 19, 19
 RATIO = 30
 
-DARK_GREEN = (0x3a, 0x5a, 0x40)
-LIGHT_GREEN = (0x58, 0x81, 0x57)
-
-screen = pygame.display.set_mode((H * RATIO, W * RATIO), pygame.NOFRAME)
+screen = pygame.display.set_mode((height * RATIO, width * RATIO), pygame.NOFRAME)
 clock = pygame.time.Clock()
 run = True
-
-def draw_checkerboard():
-    for i in range(W):
-        for j in range(H):
-            pygame.draw.rect(screen, (LIGHT_GREEN if (i + j) % 2 == 0 else DARK_GREEN), (i * RATIO, j * RATIO, RATIO, RATIO))
 
 class Ring:
     def __init__(self, x: int, y: int, next: Ring | None = None):
@@ -32,20 +26,38 @@ class Snake:
         if type(x) != int or type(y) != int:
             raise TypeError
         self.ring = Ring(x, y)
+        self.dx, self.dy = 0, 0
 
-    def update(self, key: int = 0):
+    def update(self, width: int, height: int, events: List[pygame.event.Event], fruits: List[Fruit]):
         ring = self.ring
         x, y = ring.x, ring.y
         x_, y_ = 0, 0
 
-        if key == pygame.K_UP:
-            y -= 1
-        if key == pygame.K_DOWN:
-            y += 1
-        if key == pygame.K_LEFT:
-            x -= 1
-        if key == pygame.K_RIGHT:
-            x += 1
+        for event in events:
+            if event.type == pygame.QUIT:
+                run = False
+        
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.dx = 0
+                    self.dy = -1
+                if event.key == pygame.K_DOWN:
+                    self.dx = 0
+                    self.dy = 1
+                if event.key == pygame.K_LEFT:
+                    self.dx = -1
+                    self.dy = 0
+                if event.key == pygame.K_RIGHT:
+                    self.dx = 1
+                    self.dy = 0
+        
+        x += self.dx
+        y += self.dy
+
+        for fruit in fruits:
+            if fruit.location() == (x, y):
+                fruit.shuffle(width, height)
+                self.append()
 
         while ring != None:
             x_, y_ = ring.x, ring.y
@@ -60,26 +72,45 @@ class Snake:
             ring = ring.next
     
     def append(self):
-        pass
+        ring = self.ring
+        x, y = ring.x, ring.y
+        while ring.next != None:
+            ring = ring.next
+            x, y = ring.x, ring.y
+        ring.next = Ring(x - self.dx, y - self.dy)
 
+class Fruit:
+    def __init__(self, x: int = 0, y: int = 0) -> None:
+        self.x, self.y = x, y
+    
+    def draw(self):
+        pygame.draw.rect(screen, (0xff, 0x00, 0x00), (self.x * RATIO, self.y * RATIO, RATIO, RATIO))
 
-snake = Snake(W//4, H//2)
+    def shuffle(self, width: int, height: int):
+        self.x, self.y = randint(0, width - 1), randint(0, height - 1)
+    
+    def location(self):
+        return (self.x, self.y)
+
+snake = Snake(width//4, height//2)
+fruits = [Fruit(randint(0, width - 1), randint(0, height - 1)) for i in range(1)]
 
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    
-        if event.type == pygame.KEYDOWN:
-            snake.update(event.key)
-        else:
-            snake.update()
 
-    clock.tick(60)
+    snake.update(width, height, pygame.event.get(), fruits)
+    clock.tick(10)
 
     screen.fill((0, 0, 0))
-    draw_checkerboard()
+
+    #Checkerboard
+    for i in range(width):
+        for j in range(height):
+            pygame.draw.rect(screen, ((0x58, 0x81, 0x57) if (i + j) % 2 == 0 else (0x3a, 0x5a, 0x40)), (i * RATIO, j * RATIO, RATIO, RATIO))
+
     snake.draw()
+    for fruit in fruits:
+        fruit.draw()
+
     pygame.display.flip()
 
 pygame.quit()
