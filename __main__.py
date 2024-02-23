68538f03350b74a8b54f4fbdf1b958e095d7d2bb
Sycraxe <sycraxe@gmail.com>
2024-02-23 21:30:01 +0100

    Fix type checking and add scoring and multiplayer feature

--- __main__.py
+++ __main__.py
@@ -14,6 +14,7 @@ class Color:
         if type(r) != int or type(g) != int or type(b) != int:
             raise TypeError
         self.r, self.g, self.b = r, g, b
+
     def to_tuple(self) -> tuple[int, int, int]:
         return (self.r, self.g, self.b)
 
@@ -36,13 +37,15 @@ class Ring:
             raise TypeError
         if type(ratio) != int:
             raise TypeError
+        if type(border) != int:
+            raise TypeError
         if type(color) != Color:
             raise TypeError
         pygame.draw.rect(screen, color.to_tuple(), ((self.x + border) * ratio, (self.y + border) * ratio, ratio, ratio))
 
 
 class Snake:
-    def __init__(self, x: int, y: int, color: Color, controls: Controls, rings: int = 1):
+    def __init__(self, x: int, y: int, color: Color, controls: Controls, rings: int):
         if type(x) != int or type(y) != int:
             raise TypeError
         if type(color) != Color:
@@ -60,8 +63,27 @@ class Snake:
             self.append()
         self.color = color
         self.controls = controls
+        self.score = 0
 
-    def update(self, width: int, height: int, events: list[pygame.event.Event], fruits: list[Fruit]):
+    def update(self, width: int, height: int, events: list[pygame.event.Event], fruits: list[Fruit], taken: list[tuple[int, int]]):
+        if type(width) != int or type(height) != int:
+            raise TypeError
+        if type(events) != list:
+            raise TypeError
+        for event in events:
+            if type(event) != pygame.event.Event:
+                raise TypeError
+        if type(fruits) != list:
+            raise TypeError
+        for fruit in fruits:
+            if type(fruit) != Fruit:
+                raise TypeError
+        if type(taken) != list:
+            raise TypeError
+        for loc in taken:
+            if type(loc) != tuple or len(loc) != 2 or type(loc[0]) != int or type(loc[1]) != int:
+                raise TypeError
+        
         ring = self.ring
         x, y = ring.x, ring.y
         x_, y_ = 0, 0
@@ -90,20 +112,32 @@ class Snake:
 
         if x < 0 or x >= width or y < 0 or y >= height:
             self.reset(width, height)
+            return
 
-        else:
-            for fruit in fruits:
-                if (fruit.x, fruit.y) == (x, y):
-                    fruit.shuffle(width, height)
-                    self.append()
-            
-            while ring != None:
-                x_, y_ = ring.x, ring.y
-                ring.update(x, y)
-                ring = ring.next
-                x, y = x_, y_
+        for loc in taken:
+            if (x, y) == loc and (self.dx, self.dy) != (0, 0):
+                self.reset(width, height)
+                return
+
+        for fruit in fruits:
+            if (fruit.x, fruit.y) == (x, y):
+                fruit.shuffle(width, height)
+                self.append()
+                self.score += 1
+        
+        while ring != None:
+            x_, y_ = ring.x, ring.y
+            ring.update(x, y)
+            ring = ring.next
+            x, y = x_, y_
     
     def draw(self, screen: pygame.surface.Surface, ratio: int, border: int):
+        if type(screen) != pygame.surface.Surface:
+            raise TypeError
+        if type(ratio) != int:
+            raise TypeError
+        if type(border) != int:
+            raise TypeError
         ring = self.ring
         while ring != None:
             ring.draw(screen, ratio, border, self.color)
@@ -118,19 +152,38 @@ class Snake:
         ring.next = Ring(x, y)
     
     def reset(self, width: int, height: int):
+        if type(width) != int or type(height) != int:
+            raise TypeError
         self.ring = Ring(randint(0, width - 1), randint(0, height - 1))
         for _ in range(self.rings - 1):
             self.append()
         self.dx, self.dy = 0, 0
+        self.score = 0
+    
+    def locations(self) -> list[tuple[int, int]]:
+        locations = []
+        ring = self.ring
+        while ring != None:
+            locations.append((ring.x, ring.y))
+            ring = ring.next
+        return locations
 
 class Fruit:
     def __init__(self, x: int = 0, y: int = 0):
         self.x, self.y = x, y
     
     def draw(self, screen: pygame.surface.Surface, ratio: int, border: int):
-        pygame.draw.rect(screen, (0xff, 0x00, 0x00), ((self.x + border) * ratio, (self.y + border) * ratio, ratio, ratio))
+        if type(screen) != pygame.surface.Surface:
+            raise TypeError
+        if type(ratio) != int:
+            raise TypeError
+        if type(border) != int:
+            raise TypeError
+        pygame.draw.rect(screen, (0xB3, 0x36, 0x36), ((self.x + border) * ratio, (self.y + border) * ratio, ratio, ratio))
 
     def shuffle(self, width: int, height: int):
+        if type(width) != int or type(height) != int:
+            raise TypeError
         self.x, self.y = randint(0, width - 1), randint(0, height - 1)
 
 
@@ -139,29 +192,69 @@ width, height = 19, 19
 border = 2
 ratio = 30
 
-screen = pygame.display.set_mode(((height + border) * ratio, (width + border) * ratio), pygame.NOFRAME)
+pygame.init()
+
+screen = pygame.display.set_mode(((height + 2*border) * ratio, (width + 2*border) * ratio), pygame.NOFRAME)
 clock = pygame.time.Clock()
 
-snakes = [Snake(9, 9, Color(0xFF, 0x00, 0xFF), Controls(pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT), 3)]
-fruits = [Fruit(0, 0)]
+colors = [Color(0xB3, 0x59, 0xB3), Color(0x59, 0xAB, 0xB3), Color(0x59, 0xB3, 0x92), Color(0xAA, 0xB3, 0x59)]
+controls = [Controls(pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT), 
+            Controls(pygame.K_z, pygame.K_s, pygame.K_q, pygame.K_d), 
+            Controls(pygame.K_t, pygame.K_g, pygame.K_f, pygame.K_h), 
+            Controls(pygame.K_i, pygame.K_k, pygame.K_j, pygame.K_l)]
+
+snakes = [Snake(randint(0, width - 1), randint(0, height - 1), colors[i], controls[i], 3) for i in range(2)]
+fruits = [Fruit(randint(0, width - 1), randint(0, height - 1))]
+
+font = pygame.font.Font("8bitoperator.ttf", 32)
+
+plus = font.render("+", False, (0xDF, 0xDF, 0xDF))
+minus = font.render("-", False, (0xDF, 0xDF, 0xDF))
+cross = font.render("X", False, (0xB3, 0x36, 0x36))
+credits = font.render("@sycraxe", False, (0x3a, 0x5a, 0x40))
+
+plus_rect = plus.get_rect().move((width - 0.7) * ratio, (border - 1) * ratio)
+minus_rect = minus.get_rect().move((width + 0.3) * ratio, (border - 1) * ratio)
+cross_rect = cross.get_rect().move(((border + width + 0.7)) * ratio, 0.5 * ratio)
+credits_rect = credits.get_rect().move(border * ratio, (border + height) * ratio)
 
 run = True
 
 #Main loop
 while run:
 
+    taken = []
+    for snake in snakes:
+        taken += snake.locations()
+
     events = pygame.event.get()
     for snake in snakes:
-        snake.update(width, height, events, fruits)
+        snake.update(width, height, events, fruits, taken)
+    
+    pos = pygame.mouse.get_pos()
+    for event in events:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if plus_rect.collidepoint(pos) and len(snakes) < 4:
+                snakes.append(Snake(randint(0, width - 1), randint(0, height - 1), colors[len(snakes)], controls[len(snakes)], 3))
+            if minus_rect.collidepoint(pos) and len(snakes) > 1:
+                snakes.pop()
+            if cross_rect.collidepoint(pos):
+                run = False
 
-    screen.fill((0, 0, 0))
+    screen.fill((0x49, 0x6E, 0x4C))
 
     for i in range(width):
         for j in range(height):
-            pygame.draw.rect(screen, ((0x58, 0x81, 0x57) if (i + j) % 2 == 0 else (0x3a, 0x5a, 0x40)), (i * ratio, j * ratio, ratio, ratio))
+            pygame.draw.rect(screen, ((0x58, 0x81, 0x57) if (i + j) % 2 == 0 else (0x3a, 0x5a, 0x40)), ((i + border) * ratio, (j + border) * ratio, ratio, ratio))
 
-    for snake in snakes:
+    for i, snake in enumerate(snakes):
         snake.draw(screen, ratio, border)
+        screen.blit(font.render("P" + str(i+1) + ": " + str(snake.score), False, snake.color.to_tuple()), ((border + (width // 4) * i) * ratio, (border - 1) * ratio))
+
+    if len(snakes) > 1: screen.blit(minus, minus_rect)
+    if len(snakes) < 4: screen.blit(plus, plus_rect)
+    screen.blit(cross, cross_rect)
+    screen.blit(credits, credits_rect)
 
     for fruit in fruits:
         fruit.draw(screen, ratio, border)
